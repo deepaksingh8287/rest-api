1fa2c8f988873eb0af0372327e9aee2822cd7e2a
deepaksingh8287 <deepaksingh33june@gmail.com>
2024-05-24 16:51:30 +0530

    express setup

--- server.ts
+++ server.ts
@@ -1,1 +1,8 @@
-console.log("hello world!");
+import app from "./src/app"
+const startServer = () =>{
+    const port=process.env.PORT || 3000
+    app.listen(port,()=>{
+        console.log(`server is running on port ${port}`)
+    })
+}
+startServer()
