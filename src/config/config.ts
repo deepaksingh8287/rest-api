cc8e00501b6033e12a0ebcc2d105447db2fe66aa
deepaksingh8287 <deepaksingh33june@gmail.com>
2024-05-26 12:49:18 +0530

    adding jwt token

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,7 @@ conf()
 const _config={
     port:process.env.PORT ,
     databaseURL:process.env.MONGODB_CONNECTION_STRING,
-    env:process.env.NODE_ENV
+    env:process.env.NODE_ENV,
+    jwtSecret:process.env.JWT_SECRET
 }
 export const config=Object.freeze(_config)
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,31 +1,42 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModal from "./userModal";
-import bcrypt from "bcrypt"
-const createUsers = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-    const {name,email,password} = req.body
-    
-    //validation
-    if(!name || !email || !password){
-        const error=createHttpError(400,"All Fields is required")
-        return next(error)
-    }
-    //database call
-    const user=await userModal.findOne({email:email})
-    if(user){
-        const error=createHttpError(400,"User Already Exist with this email")
-        return next(error)
-    }
-    //password=>hash
-    const hashedPassword=await bcrypt.hash(password,10)
-    //process
-    //response
-    res.send({
-        message:"User Created"
-    })
+import bcrypt from "bcrypt";
+import {sign}  from "jsonwebtoken"
+import { config } from "../config/config";
+const createUsers = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+
+  //validation
+
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All Fields is required");
+    return next(error);
+  }
+
+  //database call
+
+  const user = await userModal.findOne({ email: email });
+  if (user) {
+    const error = createHttpError(400, "User Already Exist with this email");
+    return next(error);
+  }
+
+  //password=>hash
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await userModal.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  //token generation=>jwt
+  const token=sign({sub:newUser._id},config.jwtSecret as string,{expiresIn:"7d"})
+  //process
+  //response
+  res.json({
+    accessToken:token,
+  });
 };
 export { createUsers };
