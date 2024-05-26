daef7905a8b56a02f6175454e013edabe456dc01
deepaksingh8287 <deepaksingh33june@gmail.com>
2024-05-26 13:24:05 +0530

    add commit login endpoints

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -38,20 +38,37 @@ const createUsers = async (req: Request, res: Response, next: NextFunction) => {
             password: hashedPassword,
         });
         //token generation=>jwt
-       
     } catch (error) {
-      return next(createHttpError(500, "Error while creating user."));
+        return next(createHttpError(500, "Error while creating user."));
     }
     const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-      expiresIn: "7d",
-  });
+        expiresIn: "7d",
+    });
     //process
     //response
     res.status(201).json({
         accessToken: token,
     });
 };
-const loginUser=async (req: Request, res: Response, next: NextFunction)=>{
-  const { email, password } = req.body;
-}
-export { createUsers,loginUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        const error = createHttpError(400, "All Fields is required");
+        return next(error);
+    }
+    const user = await userModal.findOne({ email: email });
+    if (!user) {
+        return next(createHttpError(404, "User not found"));
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return next(createHttpError(401, "Invalid Credentials"));
+    }
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+        expiresIn: "7d",
+    });
+    return res.json({
+        token,
+    });
+};
+export { createUsers, loginUser };
